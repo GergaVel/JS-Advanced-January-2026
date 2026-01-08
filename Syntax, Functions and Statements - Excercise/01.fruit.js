9c1b1d8acddd17992bd5fd48a877bc77debe86b2
GergaVel <gergana.velikova3@gmail.com>
2026-01-08 22:54:09 +0100

    Tasks from new lesson

---
+++ Syntax, Functions and Statements - Excercise/01.fruit.js
@@ -0,0 +1,11 @@
+function buyFruit (fruit, grams, price){
+
+let weight = grams / 1000;
+let money = weight * price;
+
+console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
+    
+}
+
+buyFruit('apple', 1563, 2.35);
+buyFruit('orange', 2500, 1.80);
