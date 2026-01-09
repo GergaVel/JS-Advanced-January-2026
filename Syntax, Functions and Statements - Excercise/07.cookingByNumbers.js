1962453c9ed992084bfc68a3af790c06e42fb968
GergaVel <gergana.velikova3@gmail.com>
2026-01-09 15:18:15 +0100

    Tasks from new lesson

--- Syntax, Functions and Statements - Excercise/07.cookingByNumbers.js
+++ Syntax, Functions and Statements - Excercise/07.cookingByNumbers.js
@@ -1,6 +1,54 @@
 function cookingByNumbers(arg1, ...params){ //convert into arr
     let num = Number(arg1);
     
+    for (let i = 0; i < params.length; i++){
+        let command = params[i]
+
+        switch(command){
+            case "chop": num = num / 2; break;
+            case "dice": num = Math.sqrt(num); break;
+            case "spice": num = num + 1; break;
+            case "bake": num = num * 3; break;
+            case "fillet": num = num * 0.8; break;
+        }
+
+        console.log(num);    
+        
+    }
 }
 
 cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake','fillet');
+
+
+//level ADVANCED
+
+function cookingByNumbers(arg1, ...params){ //convert into arr
+    const operations = {
+        chop: (num) => num / 2,
+        dice: (num) => Math.sqrt(num),
+        spice: (num) => num + 1,
+        bake: (num) => num * 3,
+        fillet: (num) => num * 0.8
+    }
+    
+    let num = Number(arg1);
+
+    for (let i = 0; i < params.length; i++){
+        let command = params[i]
+
+        // as switch can be replaced by and obj (see up)
+        // switch(command){
+        //     case "chop": num = num / 2; break;
+        //     case "dice": num = Math.sqrt(num); break;
+        //     case "spice": num = num + 1; break;
+        //     case "bake": num = num * 3; break;
+        //     case "fillet": num = num * 0.8; break;
+        // }
+        num = operations[command](num)
+        console.log(num);          
+    }
+}
+
+cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake','fillet');
---
+++ Syntax, Functions and Statements - Excercise/08.validityCheker.js
@@ -0,0 +1,33 @@
+function validityChecker(...data){
+    let x1 = Number(data[0]);
+    let x2 = Number(data[1]);
+    let y1 = Number(data[2]);
+    let y2 = Number(data[3]);
+
+    if (Number.isInteger(checkDistance(x1, y1, 0, 0))){
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);        
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);  
+    }
+    
+    if (Number.isInteger(checkDistance(x2, y2, 0, 0))){
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);        
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);  
+    }
+
+    if (Number.isInteger(checkDistance(x1, y1, x2, y2))){
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);        
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);  
+    }
+
+    function checkDistance( x1, y1, x1, y2){
+        let distanceX = x1 - x2;
+        let distanceY = y1 - y2;
+
+        return Math.sqrt(distanceX ** 2 + distanceY ** 2);
+}
+}
+
+validityChecker(2, 1, 1, 1); 
