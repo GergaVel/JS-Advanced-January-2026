8dde1d4c7cc68032076b5c08ae11fb6a02a5d32e
GergaVel <gergana.velikova3@gmail.com>
2026-01-13 07:02:43 +0100

    Tasks from new lesson

--- Arrays and Nested Arrays - Excercise/04.rotateArray.js
+++ Arrays and Nested Arrays - Excercise/04.rotateArray.js
@@ -1,7 +1,15 @@
-function solve(arr, rotations) {
- 
+function solve(arr, times) {
+    if (arr.length === 0) return;
 
+    times %= arr.length;
+
+    for (let i = 0; i < times; i++) {
+        arr.unshift(arr.pop());
+    }
+
+    return arr.join(' ');
 }
 
-solve(['1','2','3','4'],2);
-solve(['Banana','Orange','Coconut','Apple'], 15);
+
+console.log(solve(['1','2','3','4'],2));
+console.log(solve(['Banana','Orange','Coconut','Apple'], 15));
