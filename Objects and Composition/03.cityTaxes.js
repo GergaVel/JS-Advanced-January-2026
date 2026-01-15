901c3015d85d10679fbd780504bb0f2d42446126
GergaVel <gergana.velikova3@gmail.com>
2026-01-15 21:50:20 +0100

    Tasks from new lesson

--- Objects and Composition/03.cityTaxes.js
+++ Objects and Composition/03.cityTaxes.js
@@ -1,5 +1,28 @@
-function solve() {
+function cityTaxes (name, population, treasury){
+    const city = {};
 
+    city.name = name;
+    city.population = population;
+    city.treasury = treasury;
+    city.taxRate = 10;
+
+    city.collectTaxes = function(){
+        this.treasury += Math.floor(this.population * this.taxRate);
+    };
+
+    city.applyGrowth = function(percent){
+        this.population += Math.floor(this.population * percent / 100);
+    };
+
+    city.applyRecession = function(percent){
+        this.treasury -= Math.ceil(this.treasury * percent / 100)
+    }; 
+
+    return city;
 }
 
-solve(); 
+const city = cityTaxes('Tortuga', 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
