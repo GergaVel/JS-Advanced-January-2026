f439a34ea4e158cca2f03db75f93de4ebc31da6c
GergaVel <gergana.velikova3@gmail.com>
2026-01-16 13:54:44 +0100

    Tasks from new lesson

--- Objects and Composition - Exercise/02.constructionCrew.js
+++ Objects and Composition - Exercise/02.constructionCrew.js
@@ -1,5 +1,18 @@
-function constructionCrew() {
-
+function constructionCrew(worker) {
+    if(!worker.dizziness){
+        return worker; // if we have a function to be excecuted in only 1 special case === early return
+    } 
+        worker.levelOfHydrated += worker.experience * worker.weight * 0.1; 
+        worker.dizziness = false
+        return worker;
 }
 
-constructionCrew();
+const res = constructionCrew(
+    { weight: 80,
+      experience: 1,
+      levelOfHydrated: 0,
+      dizziness: true 
+    });
+
+    console.log(res);
+    
