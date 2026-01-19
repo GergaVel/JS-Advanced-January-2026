0b27739d539d6384a61ecc289d0e4ca9034582e8
GergaVel <gergana.velikova3@gmail.com>
2026-01-19 23:03:51 +0100

    Tasks from new lesson

--- DOM Introduction/06.sumTable.js
+++ DOM Introduction/06.sumTable.js
@@ -1,20 +1,32 @@
+// function sumTable() {
+//     // select first table
+//     //select all rows
+//     //filter first and last rows (headers and ouput)
+//     const rows = Array.from(document.querySelector('table').querySelectorAll('tr')).slice(1, -1);
+
+//     //for each row:
+//     // -- select colums(children) 
+//     // -- take last column
+//     // -- parse content asn num and add to sum
+    
+//     let sum = 0;
+//     for (let row of rows) {
+//         const lastCol = row.children[row.children.length - 1];
+//         sum += Number(lastCol.textContent);
+//     }
+
+//    // print output in element with id "sum"
+//     document.getElementById('sum').textContent = sum;
+// }
+
 function sumTable() {
-    // select first table
-    //select all rows
-    //filter first and last rows (headers and ouput)
     const rows = Array.from(document.querySelector('table').querySelectorAll('tr')).slice(1, -1);
 
-    //for each row:
-    // -- select colums(children) 
-    // -- take last column
-    // -- parse content asn num and add to sum
-    
     let sum = 0;
     for (let row of rows) {
         const lastCol = row.children[row.children.length - 1];
         sum += Number(lastCol.textContent);
     }
 
-   // print output in element with id "sum"
-    document.getElementById('sum').textContent = sum;
+    document.getElementById('sum').textContent = sum.toFixed(2);
 }
