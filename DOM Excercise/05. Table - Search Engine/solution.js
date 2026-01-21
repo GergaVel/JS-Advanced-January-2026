010b9bad97ef074db7dbb3e425e319387b7b844f
GergaVel <gergana.velikova3@gmail.com>
2026-01-21 21:54:21 +0100

    Tasks from new lesson

--- DOM Excercise/05. Table - Search Engine/solution.js
+++ DOM Excercise/05. Table - Search Engine/solution.js
@@ -1,36 +1,39 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
-// references is better to put here
+   document.querySelector('#searchBtn').addEventListener('click', onClick); //input field: search field
+// references is better to put here, ref to DOM element, no ref = value
 
-const searchFieldRef = document.getElementById('searchField'); // ref to DOM element, no ref = value
-const tableRowRef = document.querySelectorAll('tbody tr');
+const searchFieldRef = document.getElementById('searchField'); // ref to the searchField so we can manipulate it (take value, add value, remove value)
+const tableRowRef = document.querySelectorAll('tbody tr'); // ref to the rows: body (excl Header & Footer)
 
    function onClick() {
-   //two options: 
-   // -- text filled in the field Yes: search in the tb
-   // -- text filled in the fiels No: early return
-
-   let searchText = searchFieldRef.value;
+   let searchText = searchFieldRef.value; //takes the word we look for 
       if (!searchText){
          return; //if there is nothing in the search field -> early return
       }
    searchFieldRef.value = ""; // to empty the field after each search
+   
+   
    // extract correct info from table (skip 1st and last row aka header & footer) we need only info from table body
    // we include also tr so the calculation is more precise
    // for loop to take out each tr. When you open it it has td (table data -> each ) 
 
    for (let i = 0; i < tableRowRef.length; i++){
-      const tdRef = tableRowRef[i].querySelectorAll('td'); 
+      const tdRef = tableRowRef[i].querySelectorAll('td'); //on this row, give me all td's, vsichki kletki
          for (let col = 0; col < tdRef.length; col++){
-            const tdText = tdRef[col].textContent; //returns name, email, course under one another 3 rows groups
+            const tdText = tdRef[col].textContent; //returns name, email, course under one another 3 rows groups, 3 kletki
             
-            if (tdText.includes(searchText)){ //inclides is case sensitive
+            if (tdText.includes(searchText)){ //includes is case sensitive. checks if the cell incliudes the searched word, if yes, add class and break (don;t look further)
                tableRowRef[i].classList.add('select'); //to match (yellow)
                break;
+
             } else {
-               tableRowRef[i].classList.remove('select');
+               tableRowRef[i].classList.remove('select'); //in case there is no match, remove the select
             }
          }
+      }
    }
 }
-}
+
+
+// input & text area --> value (kutiika da pishesh neshto)
+// anything else --> textContent 
