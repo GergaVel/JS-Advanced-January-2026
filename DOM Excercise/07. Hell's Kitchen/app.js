ede269b93789cfd6e627a93bc371c59790a204df
GergaVel <gergana.velikova3@gmail.com>
2026-01-22 18:28:47 +0100

    Tasks from new lesson

--- DOM Excercise/07. Hell's Kitchen/app.js
+++ DOM Excercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,30 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
+   const textAreaRef = document.querySelector('textarea');
+   const bestRestaurant = document.querySelector('#bestRestaurant p'); // to return the <p><\p>
+   const bestWorkers = document.querySelector('#workers p');
 
    function onClick () {
-      //   TODO:
+      const data = textAreaRef.value; //returns JSON Obj
+      const restaurantData = JSON.parse(data); // returns an arr
+
+      let result = {};
+
+      for (let el of restaurantData){
+         let [name, workersData] = el.split(' - ');
+
+         if (!result.hasOwnProperty(name)){
+            result[name] = {
+               avgSalary: 0,
+               bestSalary: 0,
+               workers: []
+            }
+         }
+
+         const workers = workersData.split(', ');
+
+      }
+
       
    }
 }
