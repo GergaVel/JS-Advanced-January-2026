05e9bc1588205455605ca03771493c9c2118b666
GergaVel <gergana.velikova3@gmail.com>
2026-01-24 21:58:15 +0100

    Tasks from new lesson

--- DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const output = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', onMouseMove);
+    gradient.addEventListener('mouseout', onMouseOut);
+
+    function onMouseMove(event) {
+        const percent = Math.floor(event.offsetX / gradient.clientWidth * 100);
+        output.textContent = percent + '%';
+    }
+
+    function onMouseOut() {
+        output.textContent = '';
+    }
 }
