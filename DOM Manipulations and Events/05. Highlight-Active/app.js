69886af29b4f852951efe9d0657548d6f8358586
GergaVel <gergana.velikova3@gmail.com>
2026-01-25 11:16:49 +0100

    Tasks from new lesson

--- DOM Manipulations and Events/05. Highlight-Active/app.js
+++ DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,37 @@
+//this didn't work
 function focused() {
-    console.log('TODO:...');
+    //add event listener to nearest common parent
+    // on focusin add .focus class to input parent (the class we found in the css file)
+    // on focusout remove class
+
+    const parent = document.querySelector('div'); //returns only the first div!
+    parent.addEventListener('focusin', onFocusIn); // shte izpulnqva navsqkude, no vsushtnost samo input poletata fire focusin -focusout
+    parent.addEventListener('focusout', onFocusOut);
+
+    function onFocusIn(event) {
+        event.target.parentElement.className = 'focused';
+    }
+
+    function onFocusOut(event) {
+         event.target.parentElement.className = ' ';
+    }
+}
+
+//this worked
+
+function focused() {
+    const inputs = document.querySelectorAll('input');
+
+    for (let input of inputs) {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(event) {
+        event.target.parentElement.classList.remove('focused');
+    }
 }
