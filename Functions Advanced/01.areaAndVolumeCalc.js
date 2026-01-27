e05016cc41abb0632d3353ee01881ec993c164f9
GergaVel <gergana.velikova3@gmail.com>
2026-01-27 09:12:02 +0100

    Tasks from new lesson

---
+++ Functions Advanced/01.areaAndVolumeCalc.js
@@ -0,0 +1,32 @@
+function solve(area, vol, jsonData) {
+    const data = JSON.parse(jsonData);
+    const result = [];
+
+    
+
+    for (let entry of data){
+        const obj = {
+            area: area.call(entry), //takes the formula from area()
+            volume: vol.call(entry)
+        };
+
+        result.push(obj);
+    }
+
+    return result;
+}
+
+function area() {
+    return Math.abs(this.x * this.y);
+}
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+}
+
+const data = `[ 
+{"x":"1","y":"2","z":"10"}, 
+{"x":"7","y":"7","z":"10"}, 
+{"x":"5","y":"2","z":"10"} ]`;
+
+console.log(solve(area,vol,data)); 
