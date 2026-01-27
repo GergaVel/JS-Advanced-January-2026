c48606fea3b902012cdd459fe03ff47868c48798
GergaVel <gergana.velikova3@gmail.com>
2026-01-27 08:16:05 +0100

    Tasks from new lesson

--- DOM Manipulations and Events  Excercise/06. Furniture/solution.js
+++ DOM Manipulations and Events  Excercise/06. Furniture/solution.js
@@ -1,34 +1,37 @@
 function solve() {
+  //ref to all the items we will manipulate
   const [generateBtn, buyBtn] = document.querySelectorAll('button');
   const [input, output] = document.querySelectorAll('textarea');
-  const tableRef = document.querySelector('table tbody');
+  const tableRef = document.querySelector('table tbody'); 
 
+  //attach listeners
   generateBtn.addEventListener('click', onGenerate);
   buyBtn.addEventListener('click', onGenerateOrder);
   
+  //when we paste the data in JSON format, it parses it to JS so we can go throu it with the for loo[]
   function onGenerate() {
     const data = JSON.parse(input.value);
 
     for (let el of data){
-      const tr = createTableRow(el);
-      tableRef.appendChild(tr);
+      const tr = createTableRow(el); //this is callback to another function! Creates per row the info
+      tableRef.appendChild(tr); // append the new row to the original table
     }
 
-    input.value = '';
+    input.value = ''; //clean up
 
   }
 
-  function onGenerateOrder() {
-    const selectItem = document.querySelectorAll('input[type="checkbox"]:checked'); 
+  function onGenerateOrder() { //this is when we buy the selected items
+    const selectItem = document.querySelectorAll('input[type="checkbox"]:checked'); //all the clicked atributes
     let names = [];
     let sum = 0;
     let decFactorSum = 0;
 
-    for (const item of selectItem) {
-      const tr = item.parentElement.parentElement;
+    for (const item of selectItem) { //item is each of these checkbox items
+      const tr = item.parentElement.parentElement;// we go 2 steps above to extract the whole row
       // tr.queryselectorAll(p) OR
-      const [imgTd, nameTd, priceTd, decFactorTd] = tr.children;
-      const name = nameTd.children[0].textContent;
+      const [imgTd, nameTd, priceTd, decFactorTd] = tr.children; //destructure
+      const name = nameTd.children[0].textContent; //create the text taken from the p
       let price = priceTd.children[0].textContent;
       let decFactor = decFactorTd.children[0].textContent;
 
