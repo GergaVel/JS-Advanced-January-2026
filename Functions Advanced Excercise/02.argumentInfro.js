a620a51b3364e64f3dd52a7bb2183c665726c764
GergaVel <gergana.velikova3@gmail.com>
2026-01-28 16:51:16 +0100

    Tasks from new lesson

---
+++ Functions Advanced Excercise/02.argumentInfro.js
@@ -0,0 +1,19 @@
+function argumentInfro(...args) {
+    const result = {};
+
+    for (let el of args){
+        const type = typeof(el)
+        console.log(`${type}: ${el}`);
+        
+        if (!result.hasOwnProperty(type)){
+            result[type] = 0;
+        }           
+        result[type] += 1;
+    }
+    
+    Object.entries(result)
+    .sort(([keyA, valueA], [keyB, valueB]) => valueB - valueA)
+    .forEach(([type, count]) => console.log(`${type} = ${count}`));
+}
+
+argumentInfro('cat', 42, function () { console.log('Hello world!');});
